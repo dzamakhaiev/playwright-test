2cd535b44fda212a5fc2053bbb0d2a5e230fb055
dzamakhaiev <dzamakhaiev@star.global>
2024-06-15 15:28:40 +0300

    add tests for main page

--- pages.py
+++ pages.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, Locator
 import locators
 
 
@@ -7,19 +7,37 @@ class BasePage:
     def __init__(self, page: Page):
         self.page = page
 
-    def find_element(self, locator: str):
-        return self.page.locator(locator)
+    def find_element(self, locator: str, element: Locator = None):
+        if element:
+            return element.locator(locator)
+        else:
+            return self.page.locator(locator)
 
     def get_title(self):
         return self.page.title()
+
+    def get_current_url(self):
+        return self.page.url
 
 
 class MainPage(BasePage):
 
     url = 'https://demoqa.com/'
 
+
+
     def go_to_main_page(self):
         self.page.goto(self.url)
 
     def get_footer(self, locator=locators.MainPageLocators.FOOTER):
         return self.find_element(locator)
+
+    def get_banner(self):
+        return self.find_element(locator=locators.MainPageLocators.BANNER)
+
+    def find_banner_link(self, element, locator=locators.MainPageLocators.LINK):
+        return self.find_element(locator=locator, element=element)
+
+    def get_list_of_cards(self):
+        return self.page.locator(locators.MainPageLocators.CARD_ITEM).element_handles()
+
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -7,8 +7,28 @@ def test_main_page(browser):
     main_page = MainPage(browser.new_page())
     main_page.go_to_main_page()
     footer = main_page.get_footer()
+
+    assert main_page.get_current_url() == test_data.MainPage.EXP_URL
     assert main_page.get_title() == test_data.MainPage.EXP_TITLE
     assert footer.text_content() == test_data.MainPage.EXP_FOOTER
+
+
+def test_main_page_elements(browser):
+    main_page = MainPage(browser.new_page())
+    main_page.go_to_main_page()
+
+    elements = main_page.get_list_of_cards()
+    cards = [element.text_content() for element in elements]
+    assert cards == test_data.MainPage.EXP_CARDS
+
+
+def test_main_page_banner(browser):
+    main_page = MainPage(browser.new_page())
+    main_page.go_to_main_page()
+    banner = main_page.get_banner()
+
+    link = main_page.find_banner_link(element=banner)
+    assert link.get_attribute('href') == test_data.MainPage.EXP_BANNER_URL
 
 
 if __name__ == '__main__':
